e6db5e5326b14845198d41d561113299ea4c2f09
vikasv29 <vikasv200511@gmail.com>
2024-10-23 12:54:18 +0530

    Update 5a.py

--- 5a.py
+++ 5a.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
-plt.hist([6,8,12,16,10,20,13,15,17,7,5,9],bins=4)
+plt.hist([45,55,67,88,24,37,41,29,90,35,28,77,68],bins=4)
 plt.xlabel("Distribution of age",fontsize=16)
 plt.ylabel("Frequency",fontsize=16)
-plt.title("Vinay-1KI23CS182")
+plt.title("Vikas-1KI23CS180")
 plt.show()
